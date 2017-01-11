8081562b7ea5e454e215d0f7e2a002558ddc68a2
mermatriarchy <hailey.miller91@gmail.com>
2017-01-10 17:42:56 -0800

    add ex05

---
+++ ex05.rb
@@ -0,0 +1,16 @@
+my_name = 'Liam'
+my_age = 25
+my_height = 64 #inches
+my_weight = 116 #lbs
+my_eyes = 'green'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "He has #{my_eyes} and #{my_hair} hair."
+puts "His  teeth are usually #{my_teeth} depending on the coffee."
+
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age 
++ my_height + my_weight}."
